edc2a8537ae74ee1e6f5d858bacd90d9f1f63395
nhkmi1001 <nhkmi1001@gmail.com>
2022-10-18 09:53:50 +0900

    venv delete

--- a4_machine/views.py
+++ a4_machine/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-
+from user.models import User, Photo
 
 @login_required(login_url='/user/login/')
 def main(request):
     if request.method == 'GET':
         return render(request, 'main.html')
+    
+def fileupload(request):
+    if request.mothod == 'POST':
+        user = User()
+        user.user = request.user
+        user.save()
+        
+        for img in request.FILES.getlist('imgs'):
+            photo = Photo()
+            photo.user = user
+            photo.image = img
+            photo.save()
+        return redirect('/test/' + str(user.id))
+    
+        
