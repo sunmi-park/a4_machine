7c2b209c13c2c230ffd39974151fcb4b16ba352d
nhkmi1001 <nhkmi1001@gmail.com>
2022-10-20 22:02:12 +0900

    final?

--- user/views.py
+++ user/views.py
@@ -48,8 +48,7 @@ def login(request):
 def main(request):
     if request.method =='GET':
         return render(request, 'main.html')
-   
-   
+
     if request.method == 'POST':
         form = FileUploadForm(request.POST, request.FILES)
         if form.is_valid(): # 사진 업로드 유효성검사
