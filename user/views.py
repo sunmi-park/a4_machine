81ba6367600ca0c334b5b0b0b3d12bbb5da77fba
nhkmi1001 <nhkmi1001@gmail.com>
2022-10-21 10:03:02 +0900

    finial v2

--- user/views.py
+++ user/views.py
@@ -4,11 +4,10 @@ from .models import User, ImageModel
 from django.contrib.auth import authenticate, login as loginsession
 from .forms import FileUploadForm
 from django.contrib import auth
-from django.contrib.auth import logout
+from django.contrib.auth import logout, get_user_model
 import torch
 from django.conf import settings
 import re
-
 
 def signup(request):
     if request.method=='GET':
@@ -19,12 +18,15 @@ def signup(request):
         password = request.POST.get('password')
         passwordcheck = request.POST.get('passwordcheck')
         is_email = re.compile(r"^[a-zA-Z]+[!#$%&'*+-/=?^_`(){|}~]*[a-zA-Z0-9]*@[\w]+\.[a-zA-Z0-9-]+[.]*[a-zA-Z0-9]+$")
+        already_using_name = get_user_model().objects.filter(username=username)
         if password != passwordcheck:
             return render(request, 'signup.html', {'error': '비밀번호가 맞지 않습니다!'})
         elif username.replace(' ','') == '':
             return render(request, 'signup.html', {'error': 'username은 공백일 수 없습니다!'})
-        elif is_email.match(email) == False:  
-            return render(request, 'signup.html', {'error': 'email을 확인해 주세요!'})
+        elif is_email.match(email) == None:
+            return render(request, 'signup.html', {'error': 'email형식이 아닙니다!'})
+        elif already_using_name:
+            return render(request, 'signup.html', {'error': 'username이 이미 사용중입니다!'})
         elif password == passwordcheck:
             User.objects.create_user(username=username, password=password, email=email)
             return redirect('/login/')
